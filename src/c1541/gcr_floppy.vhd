ab1fb412091bc917ead7d395caed62ace67577e8
Stefan Voss <stefan.voss@live.de>
2026-06-14 15:43:49 +0200

    wip

--- src/c1541/gcr_floppy.vhd
+++ src/c1541/gcr_floppy.vhd
@@ -13,6 +13,7 @@
 ---------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 entity gcr_floppy is
@@ -48,21 +49,21 @@ end gcr_floppy;
 architecture struct of gcr_floppy is
 
 signal bit_clk_en  : std_logic;
-signal bit_clk_div : unsigned(7 downto 0);
-signal sync_cnt    : unsigned(5 downto 0) := (others => '0');
-signal byte_cnt    : unsigned(8 downto 0) := (others => '0');
+signal bit_clk_div : std_logic_vector(7 downto 0);
+signal sync_cnt    : std_logic_vector(5 downto 0) := (others => '0');
+signal byte_cnt    : std_logic_vector(8 downto 0) := (others => '0');
 signal byte_in     : std_logic_vector(7 downto 0);
 signal byte_out    : std_logic_vector(7 downto 0);
 signal byte_we     : std_logic;
-signal byte_addr   : unsigned(12 downto 0);
+signal byte_addr   : std_logic_vector(12 downto 0);
 signal nibble      : std_logic := '0';
-signal gcr_bit_cnt : unsigned(3 downto 0) := (others => '0');
-signal bit_cnt     : unsigned(2 downto 0) := (others => '0');
+signal gcr_bit_cnt : std_logic_vector(3 downto 0) := (others => '0');
+signal bit_cnt     : std_logic_vector(2 downto 0) := (others => '0');
 
 signal sync_in_n   : std_logic;
 signal byte_in_n   : std_logic;
 
-signal sector      : unsigned(4 downto 0) := (others => '0');
+signal sector      : std_logic_vector(4 downto 0) := (others => '0');
 signal state       : std_logic                    := '0';
 
 signal data_header : std_logic_vector(7 downto 0);
@@ -76,15 +77,15 @@ signal gcr_byte    : std_logic_vector(7 downto 0);
 signal mode_r1     : std_logic;
 signal mode_r2     : std_logic;
 
-signal old_track   : unsigned(5 downto 0);
+signal old_track   : std_logic_vector(5 downto 0);
 
 signal raw_bit_clk_en : std_logic;
-signal raw_bit_clk_div: unsigned(7 downto 0);
-signal raw_byte_cnt   : unsigned(12 downto 0);
-signal raw_bit_cnt    : unsigned( 2 downto 0);
+signal raw_bit_clk_div: std_logic_vector(7 downto 0);
+signal raw_byte_cnt   : std_logic_vector(12 downto 0);
+signal raw_bit_cnt    : std_logic_vector( 2 downto 0);
 signal raw_byte_in    : std_logic_vector( 7 downto 0);
 signal raw_byte_we    : std_logic;
-signal synced_bit_cnt : unsigned( 2 downto 0);
+signal synced_bit_cnt : std_logic_vector( 2 downto 0);
 signal shift_reg      : std_logic_vector(17 downto 0);
 signal sync_in_n_raw  : std_logic;
 signal byte_in_n_raw  : std_logic;
@@ -97,7 +98,7 @@ signal gcr_lut : gcr_array :=
 	 "10010","10011","01011","11011",
 	 "10110","10111","01111","10101");
 	 
-signal sector_max : unsigned(4 downto 0);
+signal sector_max : std_logic_vector(4 downto 0);
 
 signal gcr_byte_out   : std_logic_vector(7 downto 0);
 signal gcr_bit_out    : std_logic;
@@ -111,7 +112,7 @@ signal lfsr : std_logic_vector(3 downto 0) := "0001";
 
 begin
 
-ram_addr <=       std_logic_vector(raw_byte_cnt) when raw = '1' else std_logic_vector(byte_addr);
+ram_addr <=       raw_byte_cnt when raw = '1' else byte_addr;
 ram_we <=          raw_byte_we when raw = '1' else byte_we;
 ram_di <=     c1541_logic_dout when raw = '1' else byte_out;
 c1541_logic_din <= raw_byte_in when raw = '1' else byte_in;
@@ -120,19 +121,19 @@ sync_n <= '1' when ram_ready = '0' or mtr = '0' else
 	sync_in_n_raw when raw = '1' else
 	sync_in_n;
 
-dbg_sector <= std_logic_vector(sector);
+dbg_sector <= sector;
 
-with std_logic_vector(byte_cnt) select
+with byte_cnt select
   data_header <= 
 		X"08"                          when "000000000",
-	  "00"&track_num xor "000"&std_logic_vector(sector) xor id1 xor id2 when "000000001",
-	  "000"&std_logic_vector(sector)    when "000000010",
+	  "00"&track_num xor "000"&sector xor id1 xor id2 when "000000001",
+	  "000"&sector                    when "000000010",
 	  "00"&track_num                  when "000000011",
 	  id2                             when "000000100",
 	  id1                             when "000000101",
 	  X"0F"                           when others;
 
-with std_logic_vector(byte_cnt) select
+with byte_cnt select
 	data_body <=
 		X"07"     when "000000000",
 		data_cks  when "100000001",
@@ -157,20 +158,19 @@ with std_logic_vector(byte_cnt) select
 with state select
 	data <= data_header when '0', data_body when others;
 
-
 with nibble select
 	gcr_nibble <=
 		gcr_lut(to_integer(unsigned(data(7 downto 4)))) when '0',
 		gcr_lut(to_integer(unsigned(data(3 downto 0)))) when others;
 
-gcr_bit <= gcr_nibble(to_integer(gcr_bit_cnt));
+gcr_bit <= gcr_nibble(to_integer(unsigned(gcr_bit_cnt)));
 
-sector_max <=  to_unsigned(20, 5) when unsigned(track_num) < to_unsigned(18, 6) else
-			   to_unsigned(18, 5) when unsigned(track_num) < to_unsigned(25, 6) else
-			   to_unsigned(17, 5) when unsigned(track_num) < to_unsigned(31, 6) else
-			   to_unsigned(16, 5);
+sector_max <=  "10100" when track_num < std_logic_vector(to_unsigned(18,6)) else
+               "10010" when track_num < std_logic_vector(to_unsigned(25,6)) else
+               "10001" when track_num < std_logic_vector(to_unsigned(31,6)) else
+               "10000";
 
-gcr_bit_out <= gcr_byte_out(to_integer(not bit_cnt));
+gcr_bit_out <= gcr_byte_out(to_integer(unsigned(not bit_cnt)));
 
 with gcr_nibble_out select
 	nibble_out <= 	X"0" when "01010",--"01010",
@@ -191,20 +191,20 @@ with gcr_nibble_out select
 						X"F" when others; --"10101",			
 
 with freq select
-	bit_clk_div <= to_unsigned(103, 8) when "11",
-				   to_unsigned(111, 8) when "10",
-				   to_unsigned(119, 8) when "01",
-				   to_unsigned(127, 8) when others;
+    bit_clk_div <= x"67" when "11",
+                   x"6F" when "10",
+                   x"77" when "01",
+                   x"7F" when others;
 
 with raw_freq select
-raw_bit_clk_div <= to_unsigned(103, 8) when "11",
-				   to_unsigned(111, 8) when "10",
-				   to_unsigned(119, 8) when "01",
-				   to_unsigned(127, 8) when others;
+raw_bit_clk_div <= x"67" when "11",
+                   x"6F" when "10",
+                   x"77" when "01",
+                   x"7F" when others;
 
 process (clk32)
-	variable bit_clk_cnt : unsigned(7 downto 0) := (others => '0');
-	variable raw_bit_clk_cnt : unsigned(7 downto 0) := (others => '0');
+	variable bit_clk_cnt : std_logic_vector(7 downto 0) := (others => '0');
+	variable raw_bit_clk_cnt : std_logic_vector(7 downto 0) := (others => '0');
 begin
 	if rising_edge(clk32) then
 
@@ -221,18 +221,18 @@ begin
 				bit_clk_en <= '1';
 				bit_clk_cnt := bit_clk_div;
 			else
-				bit_clk_cnt := bit_clk_cnt - 1;
+				bit_clk_cnt := bit_clk_cnt - '1';
 			end if;
 
 			if raw_bit_clk_cnt = 0 then
 				raw_bit_clk_en <= '1';
 				raw_bit_clk_cnt := raw_bit_clk_div;
 			else
-				raw_bit_clk_cnt := raw_bit_clk_cnt - 1;
+				raw_bit_clk_cnt := raw_bit_clk_cnt - '1';
 			end if;
 
 			if ((byte_in_n = '0' and raw = '0') or (byte_in_n_raw = '0' and raw = '1')) and ram_ready = '1' then
-				if bit_clk_cnt > 16 and bit_clk_cnt < 94 then
+				if bit_clk_cnt > X"10" and bit_clk_cnt < X"5E" then
 					byte_n <= '0';
 				end if;
 			end if;
@@ -247,7 +247,7 @@ begin
 	end if;
 end process;
 
-sync_in_n_raw <= '0' when shift_reg(17 downto 8) = "11"&x"FF" and unsigned(raw_track_len) /= 0 and mode = '1' else '1';
+sync_in_n_raw <= '0' when shift_reg(17 downto 8) = "11"&x"FF" and raw_track_len /= 0 and mode = '1' else '1';
 
 -- G64 handling
 raw_read_write_process : process(clk32)
@@ -255,9 +255,9 @@ begin
 	if rising_edge(clk32) then
 		raw_byte_we <= '0';
 		if mtr = '0' or mounted = '0' or raw = '0' then
-			raw_byte_cnt <= to_unsigned(514, 13);
-			synced_bit_cnt <= (others => '0');
-			raw_bit_cnt <= (others => '0');
+			raw_byte_cnt <= '0'&x"202";
+			synced_bit_cnt <= "000";
+			raw_bit_cnt <= "000";
 			byte_in_n_raw <= '1';
 			shift_reg <= (others => '0');
 		else
@@ -270,7 +270,7 @@ begin
 					if shift_reg(10 downto 7) = "0000" then
 						shift_reg(8) <= '1';
 					end if;
-					if synced_bit_cnt = 7 then
+					if synced_bit_cnt = "111" then
 						byte_in_n_raw <= '0';
 						raw_byte_in <= shift_reg(15 downto 8);
 					end if;
@@ -278,26 +278,26 @@ begin
 					synced_bit_cnt <= synced_bit_cnt + 1;
 				end if;
 
-				if sync_in_n_raw = '0' or ram_ready = '0' or unsigned(raw_track_len) = 0 then
-					synced_bit_cnt <= (others => '0');
+				if sync_in_n_raw = '0' or ram_ready = '0' or raw_track_len = 0 then
+					synced_bit_cnt <= "000";
 				end if;
 			end if;
 
 			if raw_bit_clk_en = '1' then
-				raw_bit_cnt <= raw_bit_cnt + 1;
+				raw_bit_cnt <= raw_bit_cnt + '1';
 				if raw_bit_cnt = 0 then
 					shift_reg(7 downto 0) <= ram_do;
 				else
 					shift_reg(7 downto 0) <= shift_reg(6 downto 0) & '0';
 				end if;
 
-				if raw_bit_cnt = 7 then
-					if unsigned(raw_track_len) /= 0 then
+				if raw_bit_cnt = "111" then
+					if raw_track_len /= 0 then
 						raw_byte_we <= not mode;
 					end if;
 					raw_byte_cnt <= raw_byte_cnt + 1;
-					if raw_byte_cnt >= unsigned(raw_track_len) + 1 and unsigned(raw_track_len) /= 0 then
-						raw_byte_cnt <= to_unsigned(2, 13);
+					if raw_byte_cnt >= raw_track_len + 1 and raw_track_len /= 0 then
+						raw_byte_cnt <= '0'&x"002";
 					end if;
 				end if;
 			end if;
@@ -309,13 +309,10 @@ end process;
 read_write_process : process (clk32, raw)
 begin
 	if rising_edge(clk32) and raw = '0' then
---read_write_process : process (clk32)
---	if rising_edge(clk32) then
---	  if raw = '0' then
 
-	  old_track <= unsigned(track_num);
+	  old_track <= track_num;
 
-	  if old_track /= unsigned(track_num) then
+	  if old_track /= track_num then
 	    sector <= (others => '0'); --reset sector number on track change
 	  elsif mounted = '1' and bit_clk_en = '1' then
 
@@ -347,30 +344,30 @@ begin
 			gcr_byte        <= (others => '0');
 			data_cks        <= (others => '0');
 
-			if sync_cnt = 39 then 
+			if sync_cnt = X"27" then 
 				sync_cnt <= (others => '0');
 				sync_in_n <= '1';
 			else
-				sync_cnt <= sync_cnt + 1;
+				sync_cnt <= sync_cnt + '1';
 			end if;
 
 		end if;
 
 		if sync_in_n = '1' or mode = '0' then
 
-			gcr_bit_cnt <= gcr_bit_cnt + 1;
-			if gcr_bit_cnt = 4 then
+			gcr_bit_cnt <= gcr_bit_cnt + '1';
+			if gcr_bit_cnt = X"4" then
 				gcr_bit_cnt <= (others => '0');
 				if nibble = '1' then 
 					nibble    <= '0';
 					byte_addr <= sector & byte_cnt(7 downto 0);
-					if byte_cnt = 0 then
+					if byte_cnt = "000000000" then
 						data_cks <= (others => '0');
 					else
 						data_cks <= data_cks xor data;
 					end if;
 					if mode = '1' or (mode = '0' and autorise_count = '1') then
-						byte_cnt  <= byte_cnt + 1;
+						byte_cnt  <= byte_cnt + '1';
 					end if;
 				else
 					nibble <= '1';
@@ -378,35 +375,35 @@ begin
 						autorise_write <= '1';
 						autorise_count <= '1';
 					end if;
-					if byte_cnt >= 256 then
+					if byte_cnt >= "100000000" then
 						autorise_write <= '0';
 						autorise_count <= '0';
 					end if;
 				end if;
 			end if;
 
-			bit_cnt <= bit_cnt + 1;
+			bit_cnt <= bit_cnt + '1';
 			byte_in_n  <= '1';
-			if bit_cnt = 7 then
+			if bit_cnt = X"7" then
 				byte_in_n <= '0';
 				gcr_byte_out <= c1541_logic_dout;
 			end if;
 
 			if state = '0' then
 				-- header
-				if byte_cnt = 15 and bit_cnt = 0 then
+				if byte_cnt = "000001111" and bit_cnt = 0 then
 					sync_in_n <= '0';
 					state<= '1';
 				end if;
 			else
 				-- data
-				if byte_cnt = 273 then 
+				if byte_cnt = "100010001" then 
 					sync_in_n <= '0';
 					state <= '0';
 					if sector = sector_max then 
 						sector <= (others=>'0');
 					else
-						sector <= sector + 1;
+						sector <= sector + '1';
 					end if;
 				end if;
 			end if;
@@ -414,14 +411,14 @@ begin
 			-- demux byte from floppy (ram)			
 			gcr_byte <= gcr_byte(6 downto 0) & gcr_bit;
 
-			if bit_cnt = 7 then
+			if bit_cnt = X"7" then
 				byte_in <= gcr_byte(6 downto 0) & gcr_bit;
 			end if;
 
 			-- serialise/convert byte to floppy (ram)				
 			gcr_nibble_out <= gcr_nibble_out(3 downto 0) & gcr_bit_out;
 
-			if gcr_bit_cnt = 0 then
+			if gcr_bit_cnt = X"0" then
 				if nibble = '0' then 
 					byte_out(3 downto 0) <= nibble_out;
 				else
@@ -429,7 +426,7 @@ begin
 				end if;
 			end if;
 
-			if gcr_bit_cnt = 1 and nibble = '0' then
+			if gcr_bit_cnt = X"1" and nibble = '0' then
 				if autorise_write = '1' then
 					byte_we <= '1';
 				end if;	
@@ -439,7 +436,6 @@ begin
 
 		end if;
 	  end if;
-	--end if;
 	end if;
 end process;
 
